b536342c51fcca6f349a755fee90042ad491323b
Ivan Zura <izura@coppel.com.ar>
2018-03-26 12:32:56 -0300

    Agregando ejs, body-parser, views

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const express = require('express')
 const app = express()
 const puerto = 3000
+const bodyParser = require("body-parser")
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+app.use(express.static('views'))
+app.set("view engine","ejs")
 
 app.get('/',(req,res) => {
-  res.send('Mi buen amigo JS')
+  res.render("index",{
+            titulo: "Inicio"
+        })
 })
 
 app.listen(puerto,() => {
